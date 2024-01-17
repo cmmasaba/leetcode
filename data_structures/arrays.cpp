0ed2e7a6ca11ea917b371975949ea0c7ddb7615e
Collins Mmasaba <mmasabacollins9@gmail.com>
2024-01-17 22:15:42 +0300

    Array implementation

--- data_structures/arrays.cpp
+++ data_structures/arrays.cpp
@@ -3,4 +3,17 @@ ARRAY IN C++
 Declaration: type arrayName[arraySize]
 */
 
+#include <iostream>
 
+using namespace std;
+
+int main(){
+    /*
+    Ways of initializing an array:
+    float balance[5] = {1.0, 2.0, 3.0, 4.0, 5.0} creates an array 
+                    of size 5 and initialize the values, omitted values will be left empty
+    float balance[] = {1.0, 2.0, 3.0} creates an array of size 3
+    */
+    float balance[10] = {23.0, 12.9, 32.9, 23.9}; // uninitialized values left empty
+
+}
