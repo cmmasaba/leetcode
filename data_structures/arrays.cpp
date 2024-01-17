a9d15b74e43fdff2b8cbfe82e0a99d1fdec16db2
Collins Mmasaba <mmasabacollins9@gmail.com>
2024-01-17 22:54:18 +0300

    Passing array to function and returning array from function

--- data_structures/arrays.cpp
+++ data_structures/arrays.cpp
@@ -7,6 +7,9 @@ Declaration: type arrayName[arraySize]
 
 using namespace std;
 
+float getAverage(float param[], int size);
+float *getValues();
+
 int main(){
     /*
     Ways of initializing an array:
@@ -15,5 +18,38 @@ int main(){
     float balance[] = {1.0, 2.0, 3.0} creates an array of size 3
     */
     float balance[10] = {23.0, 12.9, 32.9, 23.9}; // uninitialized values left empty
+    // to access the value, index the array name
+    cout << balance[2] << endl;
 
+    /*
+   Returning an array from a function
+   Return a pointer to the array, declare the array as a static variable
+   in the function so that it persists after function returns
+   */
+    float *values = getValues();
+    for(int i = 0; i < 5; i++){
+        cout << *(values + i) << " ";
+    }
+    cout << endl;
+    /*
+    Passing an array as an argument to a function
+    Method 1 - void myFunc(int param[5]); pass a one dimensional array
+    Method 2 - void myFunc(int param[], int size)
+    */
+   float average = getAverage(values, 5);
+   cout << average << endl;
+}
+
+float getAverage(float param[], int size){
+    float sum, avg;
+    
+    for(int i = 0; i < size; i++){
+        sum += param[i];
+    }
+    return (avg = sum / size);
+}
+
+float *getValues(){
+    static float arr[5] = {230, 120, 100, 450, 400};
+    return arr;
 }
