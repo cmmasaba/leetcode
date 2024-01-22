c6afbb659b0a2e762a891861e1354037969af0f1
Collins Mmasaba <mmasabacollins9@gmail.com>
2024-01-22 20:50:06 +0300

    Finding the nth fibonacci number

---
+++ algorithms/fibonacci.py
@@ -0,0 +1,16 @@
+def fibonacci(num: int):
+    """
+    fibonacci: computes the nth fibonacci number
+    @param num: the nth number to compute the fibonacci of
+    
+    Returns:
+        an integer
+    """
+    a, b, c = 0, 1, 0
+    while c <= num - 2:
+       a, b = b, a + b
+       c += 1
+    yield b
+
+for num in fibonacci(10):
+    print(num)
